3be0493ddb0247e889bcc536124ee49181d741b9
Pri Garimalla <prithvigar@gmail.com>
2015-10-17 02:23:05 -0400

    Fixed lightSetter method call.

--- api.py
+++ api.py
@@ -2,21 +2,22 @@ from flask import Flask, url_for, Response
 import lightSetter
 
 app = Flask(__name__)
-lightEngine = lightSetter()
+lightEngine = lightSetter.lightSetter()
 
 @app.route('/setlights/<red>/<green>/<blue>')
 def api_setLights(red, green, blue):
-	try:
-		lightEngine.setlights((int(red), int(green), int(blue)))
-		return 'success'
-	except ValueError:
-		return 'failure'
+  try:
+    lightEngine.setLights((int(red), int(green), int(blue)))
+    return 'success'
+  except ValueError as e:
+    return 'failure'
+    print e
 
 @app.route('/setlights/<color>')
 def api_setLights_color(color):
-	lightEngine.setlights(color)
-	return success
+  lightEngine.setlights(color)
+  return success
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0')
 
